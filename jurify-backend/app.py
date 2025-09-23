e6e4395c464fc25f12e01ce1f3bdb9146b349cca
Harsh Naik <harshnaik2019@gmail.com>
2025-09-23 23:31:42 +0530

    Update app.py

--- jurify-backend/app.py
+++ jurify-backend/app.py
@@ -18,7 +18,7 @@ nltk.download('punkt_tab')
 
 # Initialize Flask app
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000", "https://localhost:3000", "https://jurify-test.vercel.app"])
+CORS(app, origins=["http://localhost:3000", "https://localhost:3000", "https://jurify-test.vercel.app, https://7crk8sexpd.us-east-1.awsapprunner.com"])
 
 # Download required NLTK data
 try:
